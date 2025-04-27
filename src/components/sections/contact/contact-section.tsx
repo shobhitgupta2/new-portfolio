06452a518082bb969ae0ecc99d4b4cafe8266666
shobhitgupta2 <0311Arcadia>
2025-04-27 17:32:47 +0530

    fixed mobile overflow

--- src/components/sections/contact/contact-section.tsx
+++ src/components/sections/contact/contact-section.tsx
@@ -8,7 +8,7 @@ export const ContactSection = () => {
       className="flex flex-col items-start justify-center w-full my-12 md:my-24"
     >
       <h2 className="text-4xl md:text-6xl my-12 px-4">get in touch</h2>
-      <div className="px-4 flex flex-row gap-4">
+      <div className="px-4 flex flex-row gap-4 flex-wrap">
         {contacts.map((contact) => (
           <ContactBubble
             key={contact.name}
