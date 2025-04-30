30f2baa6513ada82a7f86af379f7558671050959
shobhitgupta2 <0311Arcadia>
2025-04-30 22:28:47 +0530

    updated contacts

--- src/data/contacts.ts
+++ src/data/contacts.ts
@@ -8,15 +8,7 @@ export const contacts = [
     url: "https://github.com/shobhitgupta2",
   },
   {
-    name: "instagram",
-    url: "https://www.instagram.com/shobhitxgupta/",
-  },
-  {
     name: "say hello",
     url: "mailto:shobhitgupta2003@gmail.com",
-  },
-  {
-    name: "resume",
-    url: "https://drive.google.com/file/d/1j_6zeFbaULw13COCIWy4i-pcdtT2NOtM/view?usp=sharing",
   },
 ];
